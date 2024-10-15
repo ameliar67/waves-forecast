a4249912dc2dbc921f48b354af02d5f7262f6f69
Amelia Reese <108507479+ameliar67@users.noreply.github.com>
2024-10-14 20:59:07 -0700

    Move logic into module

--- src/main.py
+++ src/main.py
@@ -1,65 +1,44 @@
+import base64
+import surf_data
 import surfpy
 import sys
-import matplotlib.pyplot as plt
-import base64
-from io import BytesIO
 
 from flask import Flask, render_template
+from io import BytesIO
 
 app = Flask(__name__)
 
-ri_wave_location = surfpy.Location(41.35, -71.4, altitude=0.0, name="Westport")
-ri_wave_location.depth = 10.0
-ri_wave_location.angle = 200.0
-ri_wave_location.slope = 0.28
-atlantic_wave_model = surfpy.wavemodel.atlantic_gfs_wave_model()
+location = surfpy.Location(46.89, -124.10, altitude=0.0, name="Westport, WA")
+location.depth = 10.0
+location.angle = 200.0
+location.slope = 0.28
 
-print("Fetching GFS Wave Data")
-num_hours_to_forecast = 24  # One day forecast. Change to 384 to get a 16 day forecast
-wave_grib_data = atlantic_wave_model.fetch_grib_datas(0, num_hours_to_forecast)
-raw_wave_data = atlantic_wave_model.parse_grib_datas(ri_wave_location, wave_grib_data)
-
-if raw_wave_data:
-    data = atlantic_wave_model.to_buoy_data(raw_wave_data)
-else:
-    print("Failed to fetch wave forecast data")
+wave_forecast = surf_data.get_wave_forecast(
+    wave_location=location,
+    wind_location=location,
+    wave_model=surfpy.wavemodel.us_west_coast_gfs_wave_model(),
+)
+if not wave_forecast:
+    print("Failed to get forecast from NOAA")
     sys.exit(1)
 
-print("Fetching local weather data")
-ri_wind_location = surfpy.Location(46.89, -124.10, altitude=0.0, name="Westport")
-weather_data = surfpy.WeatherApi.fetch_hourly_forecast(ri_wind_location)
-surfpy.merge_wave_weather_data(data, weather_data)
-
-for dat in data:
-    dat.solve_breaking_wave_heights(ri_wave_location)
-    dat.change_units(surfpy.units.Units.english)
-
-maxs = [x.maximum_breaking_height for x in data]
-mins = [x.minimum_breaking_height for x in data]
-summary = [x.wave_summary.wave_height for x in data]
-times = [x.date for x in data]
-
-fig = plt.figure(figsize=(10, 6))
-ax = fig.subplots()
-ax.plot(times, maxs, c="green")
-ax.plot(times, mins, c="blue")
-ax.plot(times, summary, c="red")
-ax.set_xlabel("Date and Time")
-ax.set_ylabel("Breaking Wave Height (ft)")
-ax.grid(True)
-
+summary = [x.wave_summary.wave_height for x in wave_forecast]
 
 img = BytesIO()
+fig = surf_data.get_chart(wave_forecast, surfpy.units.Units.english)
 fig.savefig(img, format="png")
 img.seek(0)
-
-plot_url = base64.b64encode(img.getvalue()).decode("utf8")
+plot_base64_image = base64.b64encode(img.getvalue()).decode("utf8")
 
 
 @app.route("/")
-def wave_data():
+def wave_forecast():
     return render_template(
-        "index.html", info=summary, location=ri_wave_location.name, plot_url=plot_url, current_wave_height=round(summary[0])
+        "index.html",
+        info=summary,
+        location=location.name,
+        plot_url=plot_base64_image,
+        current_wave_height=round(summary[0]),
     )
 
 
---
+++ src/surf_data.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import surfpy
+
+def get_wave_forecast(
+    wave_location: surfpy.Location,
+    wind_location: surfpy.Location,
+    wave_model: surfpy.WaveModel,
+    hours_to_forecast=24,
+) -> list[surfpy.buoydata.BuoyData] | None:
+    wave_grib_data = wave_model.fetch_grib_datas(0, hours_to_forecast)
+    raw_wave_data = wave_model.parse_grib_datas(wave_location, wave_grib_data)
+
+    if not raw_wave_data:
+        return None
+
+    buoy_data = wave_model.to_buoy_data(raw_wave_data)
+    weather_data = surfpy.WeatherApi.fetch_hourly_forecast(wind_location)
+    wave_data = surfpy.merge_wave_weather_data(buoy_data, weather_data)
+
+    for d in wave_data:
+        d.solve_breaking_wave_heights(wave_location)
+
+    return wave_data
+
+def get_chart(forecast: list[surfpy.buoydata.BuoyData],
+              units: str = surfpy.units.Units.metric):
+    for d in forecast:
+        d.change_units(units)
+
+    maxs = [x.maximum_breaking_height for x in forecast]
+    mins = [x.minimum_breaking_height for x in forecast]
+    summary = [x.wave_summary.wave_height for x in forecast]
+    times = [x.date for x in forecast]
+
+    fig = plt.figure(figsize=(10, 6))
+    ax = fig.subplots()
+    ax.plot(times, maxs, c="green")
+    ax.plot(times, mins, c="blue")
+    ax.plot(times, summary, c="red")
+    ax.set_xlabel("Date and Time")
+    ax.set_ylabel(f"Breaking Wave Height ({forecast[0].unit})")
+    ax.grid(True)
+
+    return fig
