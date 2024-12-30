e23761e4ac94dece12ffcaa8e4d2da378ea5a261
Amelia Reese <108507479+ameliar67@users.noreply.github.com>
2024-12-29 16:20:42 -0800

    Add generate folium map function

---
+++ src/map.py
@@ -0,0 +1,27 @@
+import surfpy
+import folium
+from folium.plugins import MarkerCluster
+
+def generate_map() :
+
+    m = folium.Map(location=[1,1], zoom_start=2.5)
+    marker_cluster = MarkerCluster().add_to(m)
+
+    buoyStations = surfpy.BuoyStations()
+    buoyStations.fetch_stations()
+    
+    for buoyStation in buoyStations.stations:
+
+        if(buoyStation.buoy_type == "tao" or buoyStation.buoy_type == "oilrig" or buoyStation.buoy_type == "dart"):
+            continue
+        folium.Marker(
+            location=[buoyStation.location.latitude, buoyStation.location.longitude],
+            popup=folium.Popup(f"""<div id={buoyStation.location.name} onclick="goToForecastPage('{buoyStation.location.name}')">{buoyStation.location.name}</div>
+        """,
+        max_width=200
+    )
+        ).add_to(marker_cluster)
+
+    map_string = m.get_root().render()
+
+    return map_string
