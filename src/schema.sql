0b57f0e71b7ecc00350f22ce75015275a3032b71
Amelia Reese <108507479+ameliar67@users.noreply.github.com>
2024-12-30 16:38:02 -0800

    Add alerts field to cache

--- src/schema.sql
+++ src/schema.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS cache (
     key TEXT PRIMARY KEY,
     chart BLOB NOT NULL,
     expiry TIMESTAMP NOT NULL,
-    average_wave_height INT NOT NULL
+    average_wave_height INT NOT NULL,
+    alerts TEXT NOT NULL
 );
