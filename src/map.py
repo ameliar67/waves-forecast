a6775ac4ebfadf32e99751c2ed8edf0cfe03e32e
Amelia Reese <108507479+ameliar67@users.noreply.github.com>
2025-01-01 18:19:51 -0800

    Add href link for folium markers

--- src/map.py
+++ src/map.py
@@ -1,22 +1,15 @@
-import surfpy
 import folium
-import filteredLocations
 from folium.plugins import MarkerCluster
 
-def generate_map():
-
+def generate_map(stations):
     map = folium.Map(location=[1, 1], zoom_start=2.5)
     marker_cluster = MarkerCluster().add_to(map)
 
-    stations = filteredLocations.filterLocations(surfpy.BuoyStations())
-
-    for buoyStation in stations.keys():
-
+    for loc_id, buoyStation in stations.items():
         folium.Marker(
-            location=[stations[buoyStation]["latitude"], stations[buoyStation]["longitude"]],
+            location=[buoyStation["latitude"], buoyStation["longitude"]],
             popup=folium.Popup(
-                f"""<div id={buoyStation} onclick="goToForecastPage('{buoyStation}')">{buoyStation}</div>
-        """,
+                f"<div><a href='/forecast/{loc_id}'>{buoyStation['name']}</a></div>\n",
                 max_width=200,
             ),
         ).add_to(marker_cluster)
