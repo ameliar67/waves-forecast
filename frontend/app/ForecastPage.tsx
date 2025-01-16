6b62441432a80439c7027e4bcd1c874123ca59c3
Amelia Reese <108507479+ameliar67@users.noreply.github.com>
2025-01-15 16:58:29 -0800

    Reformatting and add graph label

--- frontend/app/ForecastPage.tsx
+++ frontend/app/ForecastPage.tsx
@@ -58,47 +58,48 @@ export const ForecastPage: React.FC<{ stations: Record<string, BuoyStation> }> =
 
       <div className="individual_data_fields">
         <div className="wave_wind_air">
-        <div className="wave_data_fields">
-          <div className="wave_height_layout">
-            <p className="wave_height">
-              {forecastData ? `${forecastData.current_wave_height} ${forecastData.units}` : "Loading..."}
-            </p>
-            <p className="current_wave_height_text" id="current_wave_height">
-              Wave Height
-            </p>
+          <div className="wave_data_fields">
+            <div className="wave_height_layout">
+              <p className="wave_height">
+                {forecastData ? `${forecastData.current_wave_height} ${forecastData.units}` : "Loading..."}
+              </p>
+              <p className="current_wave_height_text" id="current_wave_height">
+                Wave Height
+              </p>
+            </div>
           </div>
-        </div>
 
-        <div className="wind_data_fields">
-          <div className="wind_layout">
-            <p className="data">{forecastData ? forecastData.wind_speed : "Loading..."}</p>
-            <p className="label">Wind Speed</p>
+          <div className="wind_data_fields">
+            <div className="wind_layout">
+              <p className="data">{forecastData ? forecastData.wind_speed : "Loading..."}</p>
+              <p className="label">Wind Speed</p>
+            </div>
+            <div className="wind_layout">
+              <p className="data">{forecastData ? forecastData.wind_direction : "Loading..."}</p>
+              <p className="label">Wind Direction</p>
+            </div>
           </div>
-          <div className="wind_layout">
-            <p className="data">{forecastData ? forecastData.wind_direction : "Loading..."}</p>
-            <p className="label">Wind Direction</p>
-          </div>
-        </div>
 
-        <div className="general_weather_data_fields">
-          <div className="forecast_layout">
-            <p className="data">{forecastData ? forecastData.short_forecast : "Loading..."}</p>
-            <p className="label">Forecast</p>
+          <div className="general_weather_data_fields">
+            <div className="forecast_layout">
+              <p className="data">{forecastData ? forecastData.short_forecast : "Loading..."}</p>
+              <p className="label">Forecast</p>
+            </div>
+            <div className="forecast_layout">
+              <p className="data">{forecastData ? forecastData.air_temperature : "Loading..."}</p>
+              <p className="label">Air Temperature</p>
+            </div>
           </div>
-          <div className="forecast_layout">
-            <p className="data">{forecastData ? forecastData.air_temperature : "Loading..."}</p>
-            <p className="label">Air Temperature</p>
-          </div>
-        </div>
         </div>
 
         <div className="alerts_layout">
-            <p className="data">{forecastData ? forecastData.weather_alerts : "Loading..."}</p>
-            <p className="label">Current Weather Warnings</p>
+          <p className="data">{forecastData ? forecastData.weather_alerts : "Loading..."}</p>
+          <p className="label">Current Weather Warnings</p>
         </div>
       </div>
 
-      <div>
+      <p className="wave_height_graph_label">Wave Height</p>
+      <div className="wave_height_graph">
         {forecastData ? (
           <img src={`data:image/png;base64,${forecastData.wave_height_graph}`} alt="Wave Height Graph" />
         ) : (
