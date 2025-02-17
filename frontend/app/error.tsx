d464b8c7b14905d360ff7233bd93debb6dc386a6
Amelia Reese <108507479+ameliar67@users.noreply.github.com>
2025-02-17 12:39:12 -0800

    Add location form to error page

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -1,22 +1,40 @@
 import React from "react";
+import { LocationForm } from "./LocationForm";
+import { BuoyStation } from "./api";
 
 interface ErrorPageProps {
   errorMessage: string;
   statusCode: number;
   errorDetails?: string;
+  stations: Record<string, BuoyStation>;
+  locationId: string,
+  locationName: string,
 }
 
 export const Custom500Page: React.FC<ErrorPageProps> = ({
-  errorMessage,
-  errorDetails,
   statusCode,
+  locationName,
+  stations,
+  locationId,
+  errorMessage,
+  errorDetails
 }) => {
+
   return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>{statusCode} Page Not Found</h1>
-      <p>{errorMessage}</p>
-      {errorDetails && <pre>{errorDetails}</pre>}
-      <p>If you believe this is an error, please contact support.</p>
+    <div id="main-container">
+      <div className="forecast_header">
+        <a href="/" className="wave_and_weather_title">
+          Surf Forecast
+        </a>
+        <p className="title_form_text">{locationName}</p>
+        <LocationForm activeStationId={locationId} stations={stations} />
+      </div>
+
+      <div style={{ textAlign: "center", padding: "50px" }}>
+        <h1>{statusCode} Page Not Found</h1>
+        <p>{errorMessage}</p>
+        <pre>{errorDetails}</pre>
+      </div>
     </div>
   );
 };
