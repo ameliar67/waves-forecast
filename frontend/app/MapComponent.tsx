b601109137dd7c91b3c75f1b1a5468e1d9878083
Amelia Reese <108507479+ameliar67@users.noreply.github.com>
2025-03-09 11:56:21 -0700

    Recenter map around available locations

--- frontend/app/MapComponent.tsx
+++ frontend/app/MapComponent.tsx
@@ -20,7 +20,7 @@ export const MapComponent: React.FC<MapComponentProps> = ({
   stations,
 }) => {
   return (
-    <MapContainer className={className} center={center} zoom={2.5}>
+    <MapContainer className={className} center={[30.5, -95.5]} zoom={2.5}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
       <MarkerCluster>
