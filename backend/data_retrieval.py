65c04a5363d80689f263c00c9c10450b9a8a57a4
Amelia Reese <108507479+ameliar67@users.noreply.github.com>
2025-03-18 14:25:00 -0700

    Adjust comments

--- backend/data_retrieval.py
+++ backend/data_retrieval.py
@@ -145,14 +145,12 @@ def retrieve_new_data(wave_model, hours_to_forecast, location, conversion_rate) 
     for x in wave_data:
         hourly_forecast.append(x.wave_summary.wave_height * conversion_rate)
 
-        # Get the hour from the date
-        hour = x.date.hour  # Extracts the hour from the datetime object
+        # Remove hourly forecast later than 21:00 (9 PM) or earlier than 6:00 AM
+        hour = x.date.hour
 
-        # Check if the hour is later than 21:00 (9 PM) or earlier than 6:00 AM
         if hour > 21 or hour < 6:
             continue
 
-        # Append forecast hours and dates
         forecast_hours.append(x.date.strftime("%H:%M"))
         forecast_dates.append(x.date.strftime("%m-%d-%Y"))
 
