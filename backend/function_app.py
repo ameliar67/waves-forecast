2a0d4d71a9b3f3f811ce91eac6b78a3d86d2eb5d
Amelia Reese <108507479+ameliar67@users.noreply.github.com>
2025-04-03 16:48:53 -0700

    Use bindings for location blob client

--- backend/function_app.py
+++ backend/function_app.py
@@ -4,6 +4,7 @@ import logging
 import azure.functions as func
 import forecast_data
 from azure.storage.blob import ContentSettings
+from azurefunctions.extensions.bindings.blob import BlobClient
 from locations import get_coastal_locations
 from wave_model import get_wave_model
 
@@ -14,16 +15,17 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.function_name("RefreshLocations")
 @app.timer_trigger(schedule="15 3 * * *", run_on_startup=False, arg_name="timer")
-async def refresh_locations(timer: func.TimerRequest) -> None:
+@app.blob_input(arg_name="locationsblob", path=locations_blob_path, connection="")
+async def refresh_locations(timer: func.TimerRequest, locationsblob: BlobClient):
     logging.info("Refreshing locations response blob")
 
     # Get updated locations and upload to Blob storage
     locations_data = get_coastal_locations()
-    response_content = json.dumps({"locations": locations_data}).encode("utf-8")
+    response_content = json.dumps({"locations": locations_data})
 
     content_settings = ContentSettings(
         content_type="application/json", cache_control="public, max-age=7200"
     )
-    locations_blob_client.upload_blob(
+    locationsblob.upload_blob(
         response_content, overwrite=True, content_settings=content_settings
     )
