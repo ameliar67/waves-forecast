647e6a23ac63c4c9e1de9b3507dbf0d8181c6a3c
Amelia Reese <108507479+ameliar67@users.noreply.github.com>
2025-04-03 17:54:03 -0700

    Reorder functions

--- backend/function_app.py
+++ backend/function_app.py
@@ -17,28 +17,6 @@ refresh_forecast_queue_name = "refresh-forecast"
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 
-@app.function_name("RefreshLocations")
-@app.timer_trigger(schedule="15 3 * * *", run_on_startup=False, arg_name="timer")
-@app.blob_input(arg_name="locationsblob", path=locations_blob_path, connection="")
-async def refresh_locations(
-    timer: func.TimerRequest, locationsblob: blob_binding.BlobClient
-):
-    logging.info("Refreshing locations response blob")
-    # Convert type hint from blob binding (required by Azure Functions) to normal BlobClient (code completion)
-    locationsblob: BlobClient = locationsblob
-
-    # Get updated locations and upload to Blob storage
-    locations_data = get_coastal_locations()
-    response_content = json.dumps({"locations": locations_data})
-
-    content_settings = ContentSettings(
-        content_type="application/json", cache_control="public, max-age=7200"
-    )
-    locationsblob.upload_blob(
-        response_content, overwrite=True, content_settings=content_settings
-    )
-
-
 class ForecastQueueMessage(TypedDict):
     output_path: str
     name: str
@@ -46,7 +24,81 @@ class ForecastQueueMessage(TypedDict):
     long: float
 
 
-@app.function_name("QueueRefreshForecasts")
+@app.function_name("LocationForecast")
+@app.queue_trigger(
+    arg_name="message", queue_name=refresh_forecast_queue_name, connection=""
+)
+@app.blob_input(
+    arg_name="datacontainer",
+    path=data_container_name,
+    connection="",
+)
+async def forecast(message: str, datacontainer: blob_binding.ContainerClient) -> None:
+    logging.info(f"Refreshing forecast for {message}")
+    # Convert type hint from blob binding (required by Azure Functions) to normal ContainerClient (code completion)
+    data_container_client: ContainerClient = datacontainer
+    selected_location: ForecastQueueMessage = json.loads(message)
+
+    # Fetch wave forecast data
+    wave_model = get_wave_model(
+        selected_location["latitude"], selected_location["longitude"]
+    )
+    wave_forecast = await forecast_data.get_wave_forecast(
+        wave_model=wave_model,
+        lat=selected_location["latitude"],
+        lon=selected_location["longitude"],
+    )
+
+    # Handle missing forecast data
+    if not wave_forecast:
+        err_message = f"Failed to retrieve forecast data for {message}"
+        logging.error(err_message)
+        raise ValueError(err_message)
+
+    # Prepare response data
+    response_data = {
+        "selected_location": selected_location["name"],
+        "current_wave_height": wave_forecast["average_wave_height"],
+        "weather_alerts": wave_forecast["weather_alerts"],
+        "air_temperature": wave_forecast["air_temperature"],
+        "short_forecast": wave_forecast["short_forecast"],
+        "wind_speed": wave_forecast["wind_speed"],
+        "wind_direction": wave_forecast["wind_direction"],
+        "hourly_forecast": wave_forecast["hourly_forecast"],
+    }
+
+    forecast_blob_client = data_container_client.get_blob_client(
+        selected_location["output_path"]
+    )
+    content_settings = ContentSettings(
+        content_type="application/json", cache_control="public, max-age=1800"
+    )
+    await forecast_blob_client.upload_blob(
+        json.dumps(response_data), overwrite=True, content_settings=content_settings
+    )
+
+
+@app.function_name("RefreshLocations")
+@app.timer_trigger(schedule="15 3 * * *", run_on_startup=False, arg_name="timer")
+@app.blob_input(arg_name="locationsblob", path=locations_blob_path, connection="")
+def refresh_locations(timer: func.TimerRequest, locationsblob: blob_binding.BlobClient):
+    logging.info("Refreshing locations response blob")
+
+    # Get updated locations and upload to Blob storage
+    locations_data = get_coastal_locations()
+    response_content = json.dumps({"locations": locations_data})
+
+    # Convert type hint from blob binding (required by Azure Functions) to normal BlobClient (code completion)
+    locations_blob: BlobClient = locationsblob
+    content_settings = ContentSettings(
+        content_type="application/json", cache_control="public, max-age=7200"
+    )
+    locations_blob.upload_blob(
+        response_content, overwrite=True, content_settings=content_settings
+    )
+
+
+@app.function_name("QueueLocationForecasts")
 @app.timer_trigger(schedule="15 4 * * *", run_on_startup=False, arg_name="timer")
 @app.blob_input(
     arg_name="locationsjson",
@@ -57,15 +109,15 @@ class ForecastQueueMessage(TypedDict):
 @app.queue_output(
     arg_name="$return", queue_name=refresh_forecast_queue_name, connection=""
 )
-async def queue_refresh_forecasts(timer: func.TimerRequest, locationsjson: str):
+def queue_location_forecasts(timer: func.TimerRequest, locationsjson: str):
     locations: dict[str, LocationData] = json.loads(locationsjson)["locations"]
     queue_messages: list[str] = []
 
     for loc in locations.values():
         message: ForecastQueueMessage = {
             "output_path": f"forecast/{loc['id']}",
-            "lat": loc["latitude"],
-            "long": loc["longitude"],
+            "latitude": loc["latitude"],
+            "longitude": loc["longitude"],
             "name": loc["name"],
         }
         queue_messages.append(json.dumps(message))
@@ -75,54 +127,3 @@ async def queue_refresh_forecasts(timer: func.TimerRequest, locationsjson: str):
         queue_messages = queue_messages[:1]
 
     return queue_messages
-
-
-@app.function_name("RefreshLocationForecast")
-@app.queue_trigger(
-    arg_name="message", queue_name=refresh_forecast_queue_name, connection=""
-)
-@app.blob_input(
-    arg_name="datacontainer",
-    path=data_container_name,
-    connection="",
-)
-async def refresh_location_forecast(
-    message: str, datacontainer: blob_binding.ContainerClient
-) -> None:
-    logging.info(f"Refreshing forecast for {message}")
-    # Convert type hint from blob binding (required by Azure Functions) to normal ContainerClient (code completion)
-    datacontainer: ContainerClient = datacontainer
-    location: ForecastQueueMessage = json.loads(message)
-
-    wave_model = get_wave_model(location["lat"], location["long"])
-    wave_forecast = await forecast_data.get_wave_forecast(
-        wave_model=wave_model,
-        lat=location["lat"],
-        lon=location["long"],
-    )
-
-    if not wave_forecast:
-        err_message = f"Failed to retrieve forecast data for {message}"
-        logging.error(err_message)
-        raise ValueError(err_message)
-
-    response_content = json.dumps(
-        {
-            "selected_location": location["name"],
-            "current_wave_height": wave_forecast["average_wave_height"],
-            "weather_alerts": wave_forecast["weather_alerts"],
-            "air_temperature": wave_forecast["air_temperature"],
-            "short_forecast": wave_forecast["short_forecast"],
-            "wind_speed": wave_forecast["wind_speed"],
-            "wind_direction": wave_forecast["wind_direction"],
-            "hourly_forecast": wave_forecast["hourly_forecast"],
-        }
-    )
-
-    forecast_blob_client = datacontainer.get_blob_client(location["output_path"])
-    content_settings = ContentSettings(
-        content_type="application/json", cache_control="public, max-age=1800"
-    )
-    await forecast_blob_client.upload_blob(
-        response_content, overwrite=True, content_settings=content_settings
-    )
