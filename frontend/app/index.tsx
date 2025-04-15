b56ed3b62725a57bda652fba3c9c5c32f52fb1ab
Amelia Reese <108507479+ameliar67@users.noreply.github.com>
2025-04-14 17:18:30 -0700

    Add Inter font source

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,6 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "../styles/globals.css";
 import { App } from "./App";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/500.css";
 
 const mountElement = document.getElementById("root")!;
 const reactRoot = createRoot(mountElement);
